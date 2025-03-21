9f90d080331ceaf07798240a3666d5de67e8c700
Jakub <jakub.manko123@gmail.com>
2025-03-21 23:30:36 +0100

    Added background to login and register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -76,7 +76,7 @@ const Login = () => {
     }
 
     return (
-        <div className="mt-5 mx-auto border border-secondary rounded-5 pt-5 pb-5 text-center" style={{maxWidth: "475px"}}>
+        <div className="mt-5 mx-auto bg-body border border-secondary rounded-5 pt-5 pb-5 text-center" style={{maxWidth: "475px"}}>
             <h1 style={{fontSize: "28px"}}>Zaloguj się</h1>
             <form className="mt-4 mb-3" onSubmit={handleSubmit}>
                 <div className="mb-3">
