ce2bdae56129f23523c7c89cc55033747a8bb7b9
Jakub <jakub.manko123@gmail.com>
2025-03-24 01:02:01 +0100

    Initial main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,84 @@
-const Home = () => {
+import "../styles/Home.css";
 
+const Home = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-12 col-md-6 col-xl-4 mt-4">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col">
+                                <img className="thumbnail" src="https://i.ytimg.com/vi/_oedcuHCQwU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAzgbQf8zwIHwVWrZPqC2Q0V6OVRQ" />
+                            </div>
+                        </div>
+                        <div className="row mt-2">
+                            <div className="col">
+                                <div className="container p-0">
+                                    <div className="row">
+                                        <div className="col title">Wiedźmin 3: Dziki Gon - Jaskinia Snów - Droga ku zagładzie NG+</div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col author">Szablo Mario</div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col details">72 062 wyświetleń, 24.06.2021</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6 col-xl-4 mt-4">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col">
+                                <img className="thumbnail" src="https://i.ytimg.com/vi/_oedcuHCQwU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAzgbQf8zwIHwVWrZPqC2Q0V6OVRQ" />
+                            </div>
+                        </div>
+                        <div className="row mt-2">
+                            <div className="col">
+                                <div className="container p-0">
+                                    <div className="row">
+                                        <div className="col title">Wiedźmin 3: Dziki Gon - Jaskinia Snów - Droga ku zagładzie NG+</div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col author">Szablo Mario</div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col details">72 062 wyświetleń, 24.06.2021</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6 col-xl-4 mt-4">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col">
+                                <img className="thumbnail" src="https://i.ytimg.com/vi/_oedcuHCQwU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAzgbQf8zwIHwVWrZPqC2Q0V6OVRQ" />
+                            </div>
+                        </div>
+                        <div className="row mt-2">
+                            <div className="col">
+                                <div className="container p-0">
+                                    <div className="row">
+                                        <div className="col title">Wiedźmin 3: Dziki Gon - Jaskinia Snów - Droga ku zagładzie NG+</div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col author">Szablo Mario</div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col details">72 062 wyświetleń, 24.06.2021</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
