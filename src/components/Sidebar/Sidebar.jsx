f6f7008789b34439b27a0a7000243e2239189a23
Kuba <01178866@pw.edu.pl>
2025-07-02 14:20:42 +0200

    Added closing offcanvas while navigating

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,6 +2,12 @@ import { Link } from 'react-router-dom';
 import './Sidebar.css';
 
 const Sidebar = () => {
+    function handleCloseOffcanvas() {
+        const offcanvasEl = document.getElementById('offcanvasExample');
+        const bsOffcanvas = window.bootstrap.Offcanvas.getInstance(offcanvasEl);
+        bsOffcanvas.hide();
+    }
+
     return (
         <>
             <nav className="nav flex-column sidebar border-end d-none">
@@ -36,27 +42,27 @@ const Sidebar = () => {
                 </div>
                 <div className="offcanvas-body">
                     <nav className="nav flex-column sidebar">
-                        <Link className="nav-link active" aria-current="page" to="/">
+                        <Link className="nav-link active" aria-current="page" to="/" onClick={handleCloseOffcanvas}>
                             <i className="fa-solid fa-house"></i>
                             <span className="ms-2">Główna</span>
                         </Link>
-                        <Link className="nav-link" to="#">
+                        <Link className="nav-link" to="#" onClick={handleCloseOffcanvas}>
                             <i className="fa-solid fa-clock-rotate-left"></i>
                             <span className="ms-2">Obejrzane filmy</span>
                         </Link>
-                        <Link className="nav-link" to="#">
+                        <Link className="nav-link" to="#" onClick={handleCloseOffcanvas}>
                             <i className="fa-solid fa-video"></i>
                             <span className="ms-2">Moje filmy</span>
                         </Link>
-                        <Link className="nav-link" to="#">
+                        <Link className="nav-link" to="#" onClick={handleCloseOffcanvas}>
                             <i className="fa-solid fa-heart"></i>
                             <span className="ms-2">Polubione filmy</span>
                         </Link>
-                        <Link className="nav-link" to="#">
+                        <Link className="nav-link" to="#" onClick={handleCloseOffcanvas}>
                             <i className="fa-solid fa-bookmark"></i>
                             <span className="ms-2">Zapisane filmy</span>
                         </Link>
-                        <Link className="nav-link" to="/watch-together">
+                        <Link className="nav-link" to="/watch-together" onClick={handleCloseOffcanvas}>
                             <i className="fa-solid fa-users"></i>
                             <span className="ms-2">Wspólne oglądanie</span>
                         </Link>
